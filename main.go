189cdc6a36a0ad7cd198391f1b24fb8ed22df024
TangyOrigami <csaenz613@gmail.com>
2024-01-11 20:51:57 -0600

    working on front-end

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	http.HandleFunc("/api/content", apiContentHandler) // Handle htmx request for dynamic content
+	http.HandleFunc("/api/content", apiContentHandler) 
 
 	log.Print("Listening on localhost:3000...")
 	err := http.ListenAndServe(":3000", nil)
@@ -20,15 +20,11 @@ func main() {
 }
 
 func apiContentHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet && r.Header.Get("Hx-Request") == "true" {
-		// Handle htmx request for dynamic content
+	if r.Method == http.MethodGet && r.Header.Get("hx-Request") == "true" {
 		content := "This is dynamic content from the backend!"
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"content": content})
 		return
 	}
-
-	// Handle non-htmx requests here if needed
 	http.NotFound(w, r)
 }
-
