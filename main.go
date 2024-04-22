ffa1480fa099d8f11c5bfa22ef5fe26f9931684d
TangyOrigami <csaenz613@gmail.com>
2024-04-21 20:05:13 -0500

    figured out what FileServer is and how it works lol

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 
 	"log"
@@ -17,6 +18,7 @@ type Page struct {
 	Body  []byte
 }
 
+// TODO:
 // This needs to save to the ./templ/ folder
 
 func (p *Page) save() error {
@@ -28,64 +30,62 @@ func (p *Page) save() error {
 func loadPage(title string) (*Page, error) {
 	filename := "templ/" + title + ".html"
 	body, err := os.ReadFile(filename)
-
 	if err != nil {
 		return nil, err
 	}
-
 	return &Page{Title: title, Body: body}, nil
+
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./index.html")
+
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/")
+
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
-
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
-
 	renderTemplate(w, "view", p)
+
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
-
 	if err != nil {
 		p = &Page{Title: title}
 	}
-
 	renderTemplate(w, "edit", p)
+
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+
 }
 
 var templates = template.Must(template.ParseFiles("./static/edit.html", "./static/view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
 }
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
@@ -93,39 +93,30 @@ var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
-
 		m := validPath.FindStringSubmatch(r.URL.Path)
-
 		if m == nil {
-
 			http.NotFound(w, r)
 
 			return
-
 		}
-
 		fn(w, r, m[2])
-
 	}
-
 }
 
 // TODO:
-// Handle default view [x]
-// URL cannot handle 'this-syntax-here' [ ]
-// Incorporate styles of homepage and blog to the dynamic wiki rendering
+// Can't handle this-syntax-here
 
 func main() {
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/view", makeHandler(viewHandler))
 
-	http.HandleFunc("/static/", staticHandler)
+	http.HandleFunc("/edit", makeHandler(editHandler))
 
-	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/save", makeHandler(saveHandler))
 
-	http.HandleFunc("/edit/", makeHandler(editHandler))
+	// TODO:
+	// Make this FileServer
+	fserver := http.FileServer(http.Dir("."))
 
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	log.Fatal(http.ListenAndServe(":8080", fserver))
+	fmt.Printf("Listening on: https://localhost:8080")
 }
