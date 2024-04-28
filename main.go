1ce9045a6e47dd097a30297f642f14cd1cb57198
TangyOrigami <csaenz613@gmail.com>
2024-04-27 20:27:19 -0500

    restructuring code, big change soon

--- main.go
+++ main.go
@@ -26,54 +26,12 @@ func (p *Page) save() error {
 }
 
 func loadPage(title string) (*Page, error) {
-	// This has to use templates
 	filename := "./templ/" + title + ".html"
 	body, err := os.ReadFile(filename)
-	fmt.Printf("%s\n", body)
 	if err != nil {
 		return nil, err
 	}
 	return &Page{Title: title, Body: body}, nil
-
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-var templates = template.Must(template.ParseFiles("./static/edit.html", "./static/view.html"))
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
 
 }
 
@@ -92,13 +50,50 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+var templates = template.Must(template.ParseFiles("./static/edit.html", "./static/view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	fmt.Printf("Rendering: %s \n", tmpl)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 // TODO:
 // Learn how to use templates
 // Serve files from templ into blog.html
 // Refactor blog.html
 
 func main() {
-	// This needs to write and save to
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 
 	http.HandleFunc("/edit/", makeHandler(editHandler))
@@ -107,6 +102,6 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir(".")))
 
+	fmt.Printf("Listening on: https://localhost:8080\n")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	fmt.Printf("Listening on: https://localhost:8080")
 }
