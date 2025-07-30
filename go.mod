43940f79c71e1cb4de3a4032fe0a329b3be92dbc
Carlos <csaenz613@gmail.com>
2025-07-30 01:29:22 -0500

    full rewrite, html and css is all i need. the browser framework.

--- go.mod
+++ go.mod
@@ -1,14 +1,3 @@
 module csaenz.dev
 
 go 1.23.2
-
-require (
-	github.com/bmizerany/pat v0.0.0-20210406213842-e4b6760bdd6f
-	github.com/go-mail/mail v2.3.1+incompatible
-	github.com/joho/godotenv v1.5.1
-)
-
-require (
-	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/mail.v2 v2.3.1 // indirect
-)
--- main.go
+++ main.go
@@ -10,11 +10,15 @@ func main() {
 	// The path given to http.Dir is relative to the project directory.
 	fileServer := http.FileServer(http.Dir("."))
 	static := http.FileServer(http.Dir("./static"))
+	assets := http.FileServer(http.Dir("./assets"))
+	blog := http.FileServer(http.Dir("./blog"))
 
 	// Register the file server as the handler for all URL paths.
 	// "/" matches all request paths.
 	http.Handle("/", fileServer)
 	http.Handle("/static", static)
+	http.Handle("/assets", assets)
+	http.Handle("/blog", blog)
 
 	log.Println("Listening on :8080...")
 	// Start the server and listen on port 8080.
